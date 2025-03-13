1796ad361b0fe7ab861dc49ffc05189afd505f20
喔喔喔喔喔喔喔 <lailai487@gmail.com>
2025-03-13 20:13:30 +0800

    flux無法直接使用註解<!-- -->，讓其不work，先改回原有樣式，有空再研究如何註解

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -14,7 +14,7 @@
             <flux:navlist variant="outline">
                 <flux:navlist.group :heading="__('Platform')" class="grid">
                     <flux:navlist.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>{{ __('Dashboard') }}</flux:navlist.item>
-                    <flux:navlist.item icon="Users" :href="route('Users')" :current="request()->routeIs('users')" wire:navigate>{{ __('使用者列表') }}</flux:navlist.item>
+                    
                 </flux:navlist.group>
             </flux:navlist>
 
